0d53fd7e070ca5cefba45acae8677e68b0c9e587
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-03 11:40:03 -0800

    Day 1, part 2 -- A bit over complicated for fun

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,27 +1,74 @@
+use clap::Parser;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
 };
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    data_file: String,
+}
+
+struct Ration {
+    calories: i32,
+}
+
+#[derive(Default)]
+struct Elf {
+    rations: Vec<Ration>,
+}
+
 fn main() {
-    let file = File::open("data.txt").expect("Failed to open file");
+    let args = Args::parse();
+
+    let file = File::open(args.data_file).expect("Failed to open file");
     let reader = BufReader::new(file);
 
-    let mut max_calories_seen = 0;
-    let mut current_calories = 0;
+    let mut elves = Vec::new();
+    let mut current_elf = Elf::default();
     for line in reader.lines() {
         let line = line.expect("Failed to read line");
         if line.is_empty() {
-            if max_calories_seen < current_calories {
-                max_calories_seen = current_calories;
-            }
-            current_calories = 0;
+            elves.push(current_elf);
+            current_elf = Elf::default();
         } else {
-            current_calories += line
-                .parse::<i32>()
-                .expect("Failed to parse i32 from string");
+            current_elf.rations.push(Ration {
+                calories: line
+                    .parse::<i32>()
+                    .expect("Failed to parse i32 from string"),
+            });
         }
     }
+    if !current_elf.rations.is_empty() {
+        elves.push(current_elf);
+    }
 
-    println!("Maximum calories carried by an elf: {}", max_calories_seen);
+    elves.sort_unstable_by_key(|elf| -1 * elf.rations.iter().map(|r| r.calories).sum::<i32>());
+
+    println!(
+        "Calories carried by top 3 elves: {}, {}, {}",
+        elves
+            .get(0)
+            .unwrap()
+            .rations
+            .iter()
+            .map(|r| r.calories)
+            .sum::<i32>(),
+        elves
+            .get(1)
+            .unwrap()
+            .rations
+            .iter()
+            .map(|r| r.calories)
+            .sum::<i32>(),
+        elves
+            .get(2)
+            .unwrap()
+            .rations
+            .iter()
+            .map(|r| r.calories)
+            .sum::<i32>()
+    );
 }
