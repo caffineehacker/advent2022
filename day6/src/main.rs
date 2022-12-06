a6e056f282fedfd9e1a7bb0f89ab32c4077ad82f
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-05 22:34:01 -0800

    Day 6, part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -24,12 +24,24 @@ fn main() {
         last_four.pop_front();
         last_four.push_back(c);
     }
+
+    // For part 2, use 14 chars
+    let mut last_fourteen: VecDeque<char> = data.chars().take(14).collect();
+    for (index, c) in data.chars().enumerate().skip(14) {
+        if is_unique(&last_fourteen) {
+            println!("Index at {}", index);
+            break;
+        }
+
+        last_fourteen.pop_front();
+        last_fourteen.push_back(c);
+    }
 }
 
-fn is_unique(last_four: &VecDeque<char>) -> bool {
+fn is_unique(maybe_unique: &VecDeque<char>) -> bool {
     let mut set = HashSet::new();
-    last_four.iter().for_each(|c| {
+    maybe_unique.iter().for_each(|c| {
         set.insert(*c);
     });
-    set.len() == 4
+    set.len() == maybe_unique.len()
 }
