b94415cca3b1bebb4c8f3e9ae7c453021ee0dad6
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-09 10:25:40 -0800

    Day 9, part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -10,6 +10,8 @@ use std::{
 struct Args {
     #[arg(long)]
     data_file: String,
+    #[arg(long)]
+    knots: usize,
 }
 
 fn main() {
@@ -23,9 +25,11 @@ fn main() {
         .map(|line| line.expect("Failed to parse line"))
         .collect();
 
-    let covered_positions: Vec<(i32, i32)> = lines
+    let initial_state = vec![(0, 0); args.knots];
+
+    let tail_covered_positions: Vec<(i32, i32)> = lines
         .iter()
-        .scan(((0, 0), (0, 0)), |state, instruction| {
+        .scan(initial_state, |state, instruction| {
             let (direction, count) = instruction.split_once(' ').unwrap();
             let change = match direction {
                 "R" => (1, 0),
@@ -34,47 +38,57 @@ fn main() {
                 "L" => (-1, 0),
                 _ => panic!("Unexpected direction"),
             };
+            let knot_count = state.len();
 
-            let mut covered_states = Vec::new();
+            let mut tail_covered_states = Vec::new();
             for _ in 0..(count.parse().expect("Failed to parse count")) {
-                state.1 .0 += change.0;
-                state.1 .1 += change.1;
+                state[knot_count - 1].0 += change.0;
+                state[knot_count - 1].1 += change.1;
 
-                if ((state.0 .0 - state.1 .0) as i32).abs() > 1
-                    || ((state.0 .1 - state.1 .1) as i32).abs() > 1
-                {
-                    // Tail needs to move
-                    if state.0 .1 != state.1 .1 {
-                        state.0 .1 += if ((state.1 .1 - state.0 .1) as i32).is_negative() {
-                            -1
-                        } else {
-                            1
-                        };
+                for knot in (0..(state.len() - 1)).rev() {
+                    if ((state[knot].0 - state[knot + 1].0) as i32).abs() > 1
+                        || ((state[knot].1 - state[knot + 1].1) as i32).abs() > 1
+                    {
+                        // Tail needs to move
+                        if state[knot].1 != state[knot + 1].1 {
+                            state[knot].1 +=
+                                if ((state[knot + 1].1 - state[knot].1) as i32).is_negative() {
+                                    -1
+                                } else {
+                                    1
+                                };
+                        }
+
+                        if state[knot].0 != state[knot + 1].0 {
+                            state[knot].0 +=
+                                if ((state[knot + 1].0 - state[knot].0) as i32).is_negative() {
+                                    -1
+                                } else {
+                                    1
+                                };
+                        }
                     }
 
-                    if state.0 .0 != state.1 .0 {
-                        state.0 .0 += if ((state.1 .0 - state.0 .0) as i32).is_negative() {
-                            -1
-                        } else {
-                            1
-                        };
+                    if knot == 0 {
+                        tail_covered_states.push(state[0]);
                     }
                 }
-
-                covered_states.push(state.0);
             }
 
-            Some(covered_states)
+            Some(tail_covered_states)
         })
         .flat_map(|s| s)
         .collect();
 
-    covered_positions
+    tail_covered_positions
         .iter()
         .for_each(|p| println!("({},{})", p.0, p.1));
 
-    let unique_positions: HashSet<(i32, i32)> =
-        HashSet::from_iter(covered_positions.iter().cloned());
+    let tail_unique_positions: HashSet<(i32, i32)> =
+        HashSet::from_iter(tail_covered_positions.iter().cloned());
 
-    println!("Number of positions covered: {}", unique_positions.len());
+    println!(
+        "Number of positions covered: {}",
+        tail_unique_positions.len()
+    );
 }
