aac8d5267f8eed8407234e56cf1fc2bbc64944cd
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-17 12:38:00 -0800

    Day 17, part 2 - Not right yet, but close. Something is just a little off on my math

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -13,7 +13,7 @@ struct Args {
     data_file: String,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Rock {
     HorizontalLine,
     Plus,
@@ -51,25 +51,20 @@ enum JetPush {
 
 struct Jet {
     jet_pattern: String,
-    next_index: u32,
+    next_index: usize,
 }
 
 impl Iterator for Jet {
     type Item = JetPush;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let push = match self
-            .jet_pattern
-            .chars()
-            .nth(self.next_index as usize)
-            .unwrap()
-        {
+        let push = match self.jet_pattern.chars().nth(self.next_index).unwrap() {
             '<' => JetPush::Left,
             '>' => JetPush::Right,
             _ => panic!("Unexpected push direction"),
         };
 
-        self.next_index = (self.next_index + 1) % self.jet_pattern.len() as u32;
+        self.next_index = (self.next_index + 1) % self.jet_pattern.len();
 
         Some(push)
     }
@@ -93,18 +88,38 @@ fn main() {
     println!("Height after 1000000000000 is {}", height);
 }
 
+struct SettledState {
+    row_index: usize,
+    rocks_dropped: u64,
+    height_above_row: usize,
+    last_rock: Rock,
+    next_jet_index: usize,
+    rows: VecDeque<u8>,
+}
+
 fn run_simulation(jet_input: &str, count: usize) -> u64 {
     let mut push_iter = Jet {
         jet_pattern: jet_input.to_string(),
         next_index: 0,
     };
-    let rocks = Rocks {
+    let mut rocks = Rocks {
         next_rock: Rock::HorizontalLine,
     };
 
+    // Optimization time!
+    // We're going to start keeping track after 1000 drops, that way we're more
+    // likely to have already dealt with the fact that the start might be slightly different.
+    //
+    // We want to keep track of the last fully settled row, the state at that point and the number of rocks dropped
+    // We might be able to find a pattern that repeats from a fully dropped row to another fully dropped row.
+    let mut settled_states: Vec<SettledState> = Vec::new();
+    let mut rocks_dropped: u64 = 0;
+    let mut bonus_height = 0;
+
     let mut rows = VecDeque::new();
     rows.push_back(0b1111111u8);
-    rocks.take(count).for_each(|r| {
+    'rock_drop: while rocks_dropped < count as u64 {
+        let r = rocks.next().unwrap();
         // Note that the rock_rows are bottom first
         let mut rock_rows = match r {
             Rock::HorizontalLine => vec![0b0011110u8],
@@ -152,7 +167,50 @@ fn run_simulation(jet_input: &str, count: usize) -> u64 {
                     }
                 }
 
-                //show_tower(&rows);
+                rocks_dropped += 1;
+
+                if rocks_dropped > 1000 && bonus_height == 0 {
+                    // If the previous row is settled, lets record this state
+                    if rows[next_drop_index] | rows[next_drop_index + 1] == 0b1111111u8 {
+                        settled_states.push(SettledState {
+                            row_index: next_drop_index,
+                            rocks_dropped: rocks_dropped,
+                            height_above_row: rows.len() - next_drop_index,
+                            last_rock: r,
+                            next_jet_index: push_iter.next_index as usize,
+                            rows: rows.clone(),
+                        });
+                    }
+
+                    if settled_states.len() > 2 {
+                        if let Some((first_index, last_index)) =
+                            check_for_pattern(&settled_states, &rows)
+                        {
+                            let initial_rocks = settled_states[first_index].rocks_dropped as u64;
+                            let repeated_rocks =
+                                settled_states[last_index].rocks_dropped as u64 - initial_rocks;
+                            let repeated_height = settled_states[last_index].row_index
+                                - settled_states[first_index].row_index;
+                            let repeat_count = (count as u64 - initial_rocks) / repeated_rocks;
+                            let remaining_count =
+                                count as u64 - (repeat_count * repeated_rocks) - initial_rocks;
+                            rocks_dropped = count as u64 - remaining_count;
+                            // We already have 1 repeat since we found it
+                            bonus_height = (repeat_count as u64) * repeated_height as u64;
+                            rows = settled_states[first_index].rows.clone();
+
+                            println!("Current height: {}", rows.len() - 1);
+                            println!("Bonus height: {}", bonus_height);
+                            println!("Rocks dropped: {}", rocks_dropped);
+                            println!("Repeat count: {}", repeat_count);
+                            println!("Rocks per repeat: {}", repeated_rocks);
+                            println!("Height per repeat: {}", repeated_height);
+
+                            continue 'rock_drop;
+                        }
+                    }
+                }
+
                 break;
             }
 
@@ -187,15 +245,58 @@ fn run_simulation(jet_input: &str, count: usize) -> u64 {
 
             next_drop_index -= 1;
         }
-    });
+    }
+
+    //show_tower(&rows, 0);
 
     // We subtract 1 for the floor we added
-    (rows.len() - 1) as u64
+    (rows.len() - 1 + bonus_height as usize) as u64
 }
 
-fn show_tower(rows: &VecDeque<u8>) {
-    let starting_depth = rows.len().checked_sub(10).unwrap_or(0);
+fn check_for_pattern(
+    settled_states: &Vec<SettledState>,
+    rows: &VecDeque<u8>,
+) -> Option<(usize, usize)> {
+    // We're going to assume the first settled state will always repeat and
+    // consider the range from that settled state to the other settled states
 
+    let first_state_start = settled_states[0].row_index;
+    let mut i = 2;
+    while i < settled_states.len() {
+        let current_state_start = settled_states[i].row_index;
+        let same_rows_count = rows
+            .iter()
+            .skip(first_state_start)
+            .enumerate()
+            .take_while(|(index, r)| {
+                current_state_start + index < rows.len() && **r == rows[current_state_start + index]
+            })
+            .count();
+
+        if first_state_start + same_rows_count == current_state_start
+            && settled_states[0].last_rock == settled_states[i].last_rock
+            && settled_states[0].next_jet_index == settled_states[i].next_jet_index
+            && settled_states[0].height_above_row == settled_states[i].height_above_row
+        {
+            println!(
+                "Start {}, length {}, height above: {}, rocks dropped {} / {}",
+                first_state_start,
+                same_rows_count,
+                settled_states[0].height_above_row,
+                settled_states[0].rocks_dropped,
+                settled_states[i].rocks_dropped
+            );
+
+            return Some((0, i));
+        }
+
+        i += 1;
+    }
+
+    None
+}
+
+fn show_tower(rows: &VecDeque<u8>, starting_depth: usize) {
     for i in (starting_depth..rows.len()).rev() {
         print!("|");
         for b in (0..7).rev() {
