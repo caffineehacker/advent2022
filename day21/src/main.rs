edc6f140ba5303c9bceb631363bcc45dd6bddfbf
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-21 09:58:59 -0800

    Day 21, part 2 - Used a binary search instead of trying to build the equation

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -12,13 +12,14 @@ struct Args {
     data_file: String,
 }
 
+#[derive(Clone)]
 enum Operand {
     Monkey(String),
-    Value(i64),
+    Value(f64),
 }
 
 impl Operand {
-    fn unwrap_value(&self) -> i64 {
+    fn unwrap_value(&self) -> f64 {
         match self {
             Operand::Value(v) => *v,
             _ => panic!("Not a value"),
@@ -33,6 +34,7 @@ impl Operand {
     }
 }
 
+#[derive(Clone)]
 enum Operation {
     Add(Operand, Operand),
     Subtract(Operand, Operand),
@@ -73,9 +75,13 @@ fn main() {
 
     let result = solve_monkey("root", &monkeys);
     println!("Part 1: {}", result);
+
+    // Part 2 gets a bit more interesting...
+    let part2 = solve_part2(&monkeys);
+    println!("Part 2: {}", part2);
 }
 
-fn solve_monkey(monkey: &str, monkeys: &HashMap<String, Operation>) -> i64 {
+fn solve_monkey(monkey: &str, monkeys: &HashMap<String, Operation>) -> f64 {
     match &monkeys[monkey] {
         Operation::Add(o1, o2) => {
             solve_monkey(o1.unwrap_monkey(), &monkeys) + solve_monkey(o2.unwrap_monkey(), &monkeys)
@@ -91,4 +97,63 @@ fn solve_monkey(monkey: &str, monkeys: &HashMap<String, Operation>) -> i64 {
         }
         Operation::Immediate(o1) => o1.unwrap_value(),
     }
+}
+
+fn solve_part2(monkeys: &HashMap<String, Operation>) -> f64 {
+    // Start with a large change to detect differences
+    let mut changing_value = 10000.0;
+    let mut humn1_monkeys = monkeys.clone();
+    *humn1_monkeys.get_mut("humn").unwrap() = Operation::Immediate(Operand::Value(1.0));
+    let mut humn2_monkeys = monkeys.clone();
+    *humn2_monkeys.get_mut("humn").unwrap() = Operation::Immediate(Operand::Value(changing_value));
+
+    let (root1, root2) = match &monkeys["root"] {
+        Operation::Add(r1, r2) => (r1.unwrap_monkey(), r2.unwrap_monkey()),
+        Operation::Subtract(r1, r2) => (r1.unwrap_monkey(), r2.unwrap_monkey()),
+        Operation::Multiply(r1, r2) => (r1.unwrap_monkey(), r2.unwrap_monkey()),
+        Operation::Divide(r1, r2) => (r1.unwrap_monkey(), r2.unwrap_monkey()),
+        Operation::Immediate(_) => panic!("Not expecting immediate value"),
+    };
+
+    let solution1_r1 = solve_monkey(root1, &humn1_monkeys);
+    let solution1_r2 = solve_monkey(root2, &humn1_monkeys);
+
+    let solution2_r1 = solve_monkey(root1, &humn2_monkeys);
+    let solution2_r2 = solve_monkey(root2, &humn2_monkeys);
+
+    // We only care about dialing in the monkey that changed
+    let changing_monkey = if solution1_r1 == solution2_r1 {
+        root2
+    } else {
+        root1
+    };
+    let target_value = if solution1_r1 == solution2_r1 {
+        solution1_r1
+    } else {
+        solution1_r2
+    };
+
+    // Binary search for the solution
+
+    let mut magnitude = 100000000000.0;
+    changing_value = 1.0;
+    *humn2_monkeys.get_mut("humn").unwrap() = Operation::Immediate(Operand::Value(changing_value));
+    let mut current_value = solve_monkey(changing_monkey, &humn2_monkeys);
+    let mut last_diff_direction =
+        (current_value - target_value) / (current_value - target_value).abs();
+    while current_value != target_value {
+        if current_value > target_value && last_diff_direction < 0.0 {
+            magnitude = -magnitude / 2.0;
+        } else if current_value < target_value && last_diff_direction > 0.0 {
+            magnitude = -magnitude / 2.0;
+        }
+
+        changing_value += magnitude;
+        *humn2_monkeys.get_mut("humn").unwrap() =
+            Operation::Immediate(Operand::Value(changing_value));
+        last_diff_direction = (current_value - target_value) / (current_value - target_value).abs();
+        current_value = solve_monkey(changing_monkey, &humn2_monkeys);
+    }
+
+    changing_value
 }
